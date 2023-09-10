0aa49770ff817d1af5be7e33b59ad2d209d5667d
PrabalCode01 <prabals476@gmail.com>
2023-09-10 22:39:23 +0530

    first commit

--- src/components/misscellaneous/UpdateGroupChatModal.js
+++ src/components/misscellaneous/UpdateGroupChatModal.js
@@ -205,23 +205,23 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
 
   return (
     <>
-      <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+      <IconButton display={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
 
       <Modal onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader
             fontSize="35px"
-            fontFamily="Work sans"
-            d="flex"
+            fontFamily="cursive"
+            display="flex"
             justifyContent="center"
           >
             {selectedChat.chatName}
           </ModalHeader>
 
           <ModalCloseButton />
-          <ModalBody d="flex" flexDir="column" alignItems="center">
-            <Box w="100%" d="flex" flexWrap="wrap" pb={3}>
+          <ModalBody display="flex" flexDir="column" alignItems="center">
+            <Box w="100%" display="flex" flexWrap="wrap" pb={3}>
               {selectedChat.users.map((u) => (
                 <UserBadgeItem
                   key={u._id}
@@ -238,15 +238,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
                 value={groupChatName}
                 onChange={(e) => setGroupChatName(e.target.value)}
               />
-              <Button
-                variant="solid"
-                colorScheme="teal"
-                ml={1}
-                isLoading={renameloading}
-                onClick={handleRename}
-              >
-                Update
-              </Button>
+            
             </FormControl>
             <FormControl>
               <Input
@@ -255,6 +247,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
                 onChange={(e) => handleSearch(e.target.value)}
               />
             </FormControl>
+           
 
             {loading ? (
               <Spinner size="lg" />
@@ -268,7 +261,19 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
               ))
             )}
           </ModalBody>
-          <ModalFooter>
+          <ModalFooter display="flex" justifyContent="space-between">
+          <Button
+                variant="solid"
+                // colorScheme="teal"
+
+              bg="lightgreen"
+              
+                mb={2}
+                isLoading={renameloading}
+                onClick={handleRename}
+              >
+                Update
+              </Button>
             <Button onClick={() => handleRemove(user)} colorScheme="red">
               Leave Group
             </Button>
